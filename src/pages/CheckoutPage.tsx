c6b8118428c3a1da021fd8b21cf4bb68330d4e6e
janeshdev01 <janesh.dev01@gmail.com>
2025-06-20 00:24:19 +0530

    Updated CheckoutPage.tsx

--- src/pages/CheckoutPage.tsx
+++ src/pages/CheckoutPage.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { CreditCard, Shield, Loader2 } from 'lucide-react';
-import { useAuth } from '../../context/AuthContext';
+import { useAuth } from '../context/AuthContext';
 import { useCart } from '../../context/CartContext';
 import Button from '../ui/Button';
 import toast from 'react-hot-toast';
