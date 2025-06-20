083784ab5ec4664a41cd0b209ffbb8f7a2af0888
janeshdev01 <janesh.dev01@gmail.com>
2025-06-20 23:33:51 +0530

    Updated utils.ts

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -8,7 +8,7 @@ export function cn(...inputs: ClassValue[]) {
 export function formatCurrency(amount: number): string {
   return new Intl.NumberFormat('en-IN', {
     style: 'currency',
-    currency: 'INR',
+    currency: 'USD',
     minimumFractionDigits: 0,
     maximumFractionDigits: 0,
   }).format(amount)
